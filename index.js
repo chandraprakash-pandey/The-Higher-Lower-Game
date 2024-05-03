34cebef12684ed6e620dcbc65f17131e5aed5950
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-05-04 00:26:22 +0530

    Added popup

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ score.innerHTML = x;
 let higher_click = document.querySelector(".higher");
 let lower_click = document.querySelector(".lower");
 
-
 lower_click.addEventListener("click", function (){
     x++;
     score.innerHTML = x;
+    score_pop();
 })
+
+function score_pop(){
+    let score_ = document.querySelector(".score");
+    score_.classList.add("score_pop");
+    setTimeout(function(){
+        score_.classList.remove("score_pop");
+    }, 300)
+}
