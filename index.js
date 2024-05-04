8ee0e83bd561ce11a46cbe50a8c10286fcb2f09c
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-05-04 14:05:41 +0530

    Red Green vs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let x = 0;
 let score = document.querySelector('.num');
 score.innerHTML = x;
-
+let vs = document.querySelector(".vs_content");
 let higher_click = document.querySelector(".higher");
 let lower_click = document.querySelector(".lower");
+
+let arr_img = [];
 
 lower_click.addEventListener("click", function (){
     x++;
@@ -14,7 +16,17 @@ lower_click.addEventListener("click", function (){
 function score_pop(){
     let score_ = document.querySelector(".score");
     score_.classList.add("score_pop");
+    vs.classList.add("vs_right");
     setTimeout(function(){
         score_.classList.remove("score_pop");
-    }, 300)
+        vs.classList.remove("vs_right");
+    }, 600)
+
 }
+
+higher_click.addEventListener("click", function (){
+    vs.classList.add("vs_wrong");
+    setTimeout(function(){
+    vs.classList.remove("vs_wrong");
+    }, 600)
+})
