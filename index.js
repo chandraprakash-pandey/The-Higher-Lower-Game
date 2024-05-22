83f308e74d353233ed01e93d3c037ba21e7ff360
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-05-22 19:02:09 +0530

    Added Images

--- index.js
+++ index.js
@@ -3,41 +3,39 @@ let score = document.querySelector('.num');
 let vs = document.querySelector(".vs_content");
 let higher_click = document.querySelector(".higher");
 let lower_click = document.querySelector(".lower");
-let right = document.querySelector(".right");
+let right_img = document.querySelector(".right");
+let left_img = document.querySelector(".left");
 
 const group = [
-    {name: '"Outlook"', searches: 68000000},
-    {name: '"News"', searches: 16000000},
-    {name: '"Cleaner"', searches: 1220000},
-    {name: '"Twitter"', searches: 83100000},
-    {name: '"New York"', searches: 2240000},
-    {name: '"Serena Williams"', searches: 1830000},
-    {name: '"Toyota"', searches: 6120000},
-    {name: '"Katy Perry"', searches: 4090000},
-    {name: '"Popeye"', searches: 1830000},
-    {name: '"Black Hole"', searches: 368000},
-    {name: '"Angry Birds"', searches: 9140000},
-    {name: '"Periodic Table"', searches: 3350000},
-    {name: '"Lego"', searches: 6120000},
-    {name: '"American Beauty"', searches: 368000},
-    {name: '"Bangkok"', searches: 1000000},
-    {name: '"John Cena"', searches: 2240000},
-    {name: '"fruit Ninja"', searches: 1500000},
-    {name: '"Queen Victoria"', searches: 823000},
-    {name: '"Nelson Mandela"', searches: 1000000},
-    {name: '"Instagram"', searches: 151000000},
-    {name: '"Alcohol"', searches: 673000},
+    {name: '"Outlook"', searches: 68000000, images: 'Images/Outlook.webp'},
+    {name: '"News"', searches: 16000000, images: 'Images/News.webp'},
+    {name: '"Cleaner"', searches: 1220000, images: 'Images/Cleaner.webp'},
+    {name: '"Twitter"', searches: 83100000, images: 'Images/Twitter'},
+    {name: '"New York"', searches: 2240000, images: 'Images/New York'},
+    {name: '"Serena Williams"', searches: 1830000, images: 'Images/Serena Williams.webp'},
+    {name: '"Toyota"', searches: 6120000, images: 'Images/Toyota.webp'},
+    {name: '"Katy Perry"', searches: 4090000, images: 'Images/Katy Perry.webp'},
+    {name: '"Popeye"', searches: 1830000, images: 'Images/Popeye.webp'},
+    {name: '"Black Hole"', searches: 368000, images: 'Images/Black Hole.webp'},
+    {name: '"Angry Birds"', searches: 9140000, images: 'Images/.webp'},
+    {name: '"Periodic Table"', searches: 3350000, images: 'Images/.webp'},
+    {name: '"Lego"', searches: 6120000, images: 'Images/.webp'},
+    {name: '"American Beauty"', searches: 368000, images: 'Images/.webp'},
+    {name: '"Bangkok"', searches: 1000000, images: 'Images/.webp'},
+    {name: '"John Cena"', searches: 2240000, images: 'Images/.webp'},
+    {name: '"fruit Ninja"', searches: 1500000, images: 'Images/.webp'},
+    {name: '"Queen Victoria"', searches: 823000, images: 'Images/.webp'},
+    {name: '"Nelson Mandela"', searches: 1000000, images: 'Images/.webp'},
+    {name: '"Instagram"', searches: 151000000, images: 'Images/.webp'},
+    {name: '"Alcohol"', searches: 673000, images: 'Images/.webp'},
 ];
 
-console.log(group[0].name);
+score.innerHTML = x;
 
-let left_img = document.querySelector(".left");
-let right_img = document.querySelector(".right");
+console.log(group[0].images);
 
-const img = [
-    'Addidas.jpg',
-    'Harry-Potter.webp'
-]
+right_img.style.backgroundImage = `url("${group[0].images}")`;
+
 
 lower_click.addEventListener("click", function (){
     x++;
